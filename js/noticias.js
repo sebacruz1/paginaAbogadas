4a6fc8c74c365410cec13f72786e5b1d5fb58549
sebacruz1 <sebastiancruzpomar@gmail.com>
2024-12-10 00:04:45 -0300

    arreglo de noticias

--- js/noticias.js
+++ js/noticias.js
@@ -1,12 +1,20 @@
 async function fetchNews() {
   try {
     const response = await fetch('http://localhost:3000/news');
+    const title = document.querySelector('.title');
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     const data = await response.json();
 
     if (data.length > 0) {
       displayNews(data);
     } else {
       console.error('No news available');
+      title.textContent = 'No hay noticias disponibles'; // Update title to indicate no news
+      return;
     }
   } catch (error) {
     console.error('Error fetching news:', error);
@@ -15,17 +23,22 @@ async function fetchNews() {
 
 function displayNews(articles) {
   const newsContainer = document.getElementById('news-container');
+
+
+
   newsContainer.innerHTML = '';
+  
+  title.textContent = 'Últimas Noticias';
 
   articles.forEach(article => {
-    const articleElement = document.createElement('div');
-    articleElement.classList.add('news-item');
-    articleElement.innerHTML = `
-        <h2><a href="${article.link}" target="_blank">${article.title}</a></h2>
-        <p>${article.description}</p>
-        <p><small>${article.source_name ? article.source_name : 'Unknown Source'} - ${new Date(article.pubDate).toLocaleDateString()}</small></p>
+      const articleElement = document.createElement('div');
+      articleElement.classList.add('news-item');
+      articleElement.innerHTML = `
+          <h2><a href="${article.link}" target="_blank">${article.title}</a></h2>
+          <p>${article.description}</p>
+          <p><small>${article.source_name ? article.source_name : 'Fuente desconocida'} - ${new Date(article.pubDate).toLocaleDateString()}</small></p>
       `;
-    newsContainer.appendChild(articleElement);
+      newsContainer.appendChild(articleElement);
   });
 }
 
