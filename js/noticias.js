2c8d9a4d98056d9dbcd6b7677403c336090f7e83
sebacruz1 <sebastiancruzpomar@gmail.com>
2024-12-10 23:13:41 -0300

    cambios en la pagina

--- js/noticias.js
+++ js/noticias.js
@@ -1,7 +1,7 @@
 async function fetchNews() {
+  const title = document.querySelector('.title');
   try {
     const response = await fetch('http://localhost:3000/news');
-    const title = document.querySelector('.title');
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -10,6 +10,7 @@ async function fetchNews() {
     const data = await response.json();
 
     if (data.length > 0) {
+      title.textContent = 'Últimas Noticias';
       displayNews(data);
     } else {
       console.error('No news available');
@@ -17,28 +18,34 @@ async function fetchNews() {
       return;
     }
   } catch (error) {
+    title.textContent = 'No hay noticias disponibles';
     console.error('Error fetching news:', error);
+
   }
 }
 
 function displayNews(articles) {
   const newsContainer = document.getElementById('news-container');
 
-
-
   newsContainer.innerHTML = '';
-  
-  title.textContent = 'Últimas Noticias';
 
   articles.forEach(article => {
-      const articleElement = document.createElement('div');
-      articleElement.classList.add('news-item');
-      articleElement.innerHTML = `
+    const articleElement = document.createElement('div');
+    articleElement.classList.add('news-item');
+    let truncatedDescription = article.description.split(' ').slice(0, 100).join(' ');
+    if (article.description.split(' ').length > 100) {
+      truncatedDescription += '...';
+    }
+    articleElement.innerHTML = `
           <h2><a href="${article.link}" target="_blank">${article.title}</a></h2>
-          <p>${article.description}</p>
-          <p><small>${article.source_name ? article.source_name : 'Fuente desconocida'} - ${new Date(article.pubDate).toLocaleDateString()}</small></p>
+          <p>${truncatedDescription}</p>
+          <p><small>${article.source_name ? article.source_name : 'Fuente desconocida'} - ${new Date(article.pubDate).toLocaleDateString('en-GB', {
+      day: '2-digit',
+      month: '2-digit',
+      year: '2-digit'
+    })}</small></p>
       `;
-      newsContainer.appendChild(articleElement);
+    newsContainer.appendChild(articleElement);
   });
 }
 
